a3e33bc2c150b2238a86bb62c7fb45770eca180d
illusion <78088291+illusionhuan@users.noreply.github.com>
2023-09-11 16:33:30 +0800

    微服务项目

--- microservice/cloud_customer_order/src/main/java/com/java/order/OrderMain.java
+++ microservice/cloud_customer_order/src/main/java/com/java/order/OrderMain.java
@@ -1,11 +1,15 @@
 package com.java.order;
 
+import com.java.order.config.ApplicationContextConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@LoadBalancerClient(name = "service-product", configuration = ApplicationContextConfig.class)
 public class OrderMain {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain.class,args);
--- microservice/cloud_customer_order/src/main/java/com/java/order/config/ApplicationContextConfig.java
+++ microservice/cloud_customer_order/src/main/java/com/java/order/config/ApplicationContextConfig.java
@@ -1,16 +1,24 @@
 package com.java.order.config;
 
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ReactorServiceInstanceLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApplicationContextConfig {
     @Bean
     //RestTemplate的负载均衡能力
-    @LoadBalanced
-    public RestTemplate getRestTemplate(){
-        return new RestTemplate();
+    public ReactorServiceInstanceLoadBalancer reactorServiceInstanceLoadBalancer(Environment environment, LoadBalancerClientFactory loadBalancerClientFactory){
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+
+        //返回随机轮询负载均衡方式
+        return new RandomLoadBalancer(loadBalancerClientFactory.getLazyProvider(name,ServiceInstanceListSupplier.class),name);
     }
 }
